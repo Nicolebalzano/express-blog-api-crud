fcf0c96d40c91ebd756d23ab0f88adc41d71b40e
Nicolebalzano <nicole.balzano03@gmail.com>
2025-05-28 16:21:40 +0200

    feat:errorHandler implemented

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,7 @@
 function errorHandler(err, req, res, next) {
+    console.log("error", err)
     res.status(500);
-    return res.jason({
+    return res.json({
         error : "errore interno al server",
     })
 }
